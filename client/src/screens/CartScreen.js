8d36237f1ee31d81b363b1cbc093983e79ccfbf5
HetavShah <hetavshah1705@outlook.com>
2023-03-31 17:55:24 +0530

    Changed $ to Rs Symbol

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -68,7 +68,7 @@ const CartScreen = ({}) => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.title}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>₹{item.price}</Col>
                   <Col md={2}>
                     <Form.Control
                       as="select"
@@ -109,7 +109,7 @@ const CartScreen = ({}) => {
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 items
               </h2>
-              $
+              ₹
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -105,7 +105,7 @@ const ProductScreen = ({}) => {
                     <Row>
                       <Col>Price:</Col>
                       <Col>
-                        <strong>${product.price}</strong>
+                        <strong>₹{product.price}</strong>
                       </Col>
                     </Row>
                   </ListGroup.Item>
