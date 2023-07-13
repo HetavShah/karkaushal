a63783ccd9c88cd8eb65e0900a8677eb7902df98
HetavShah <hetavshah1705@outlook.com>
2023-07-13 10:01:28 +0530

    Github logo in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <div>
           Copyright &copy; KarKaushal
           </div>
-          <a href="https://www.github.com/HetavShah/karkaushal">Github</a>
+          <a href="https://www.github.com/HetavShah/karkaushal"><img src="github-mark.png" height={25} width={25}></img></a>
           </Col>
           {/* <Col className="text-left py-3"><a href="https://www.github.com/HetavShah/karkaushal">Github</a></Col> */}
         </Row>
